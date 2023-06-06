c6ab4a056c1161ca55613befceafc3576d67f70c
Sanju Thenuwara <sthendev@gmail.com>
2023-06-06 22:27:30 +1000

    gitsigns: update bindings

--- lua/setup/gitsigns.lua
+++ lua/setup/gitsigns.lua
@@ -13,30 +13,19 @@ function M.setup()
                 utils.bind(mode, l, r, opts)
             end
 
-            -- Navigation
-            map('n', '<leader>gk', function()
-              vim.schedule(function() gs.prev_hunk() end)
-              return '<Ignore>'
-            end, {expr=true})
-
-            map('n', '<leader>gj', function()
-              vim.schedule(function() gs.next_hunk() end)
-              return '<Ignore>'
-            end, {expr=true})
-
             -- Actions
             map('n', '<leader>gk', gs.prev_hunk)
             map('n', '<leader>gj', gs.next_hunk)
-            -- map('n', '<leader>hs', gs.stage_hunk)
-            -- map('n', '<leader>hr', gs.reset_hunk)
-            -- map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line("."), vim.fn.line("v")} end)
-            -- map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line("."), vim.fn.line("v")} end)
-            -- map('n', '<leader>hS', gs.stage_buffer)
-            -- map('n', '<leader>hu', gs.undo_stage_hunk)
-            -- map('n', '<leader>hR', gs.reset_buffer)
+            -- map('n', '<leader>gs', gs.stage_hunk)
+            -- map('n', '<leader>gr', gs.reset_hunk)
+            -- map('v', '<leader>gs', function() gs.stage_hunk {vim.fn.line("."), vim.fn.line("v")} end)
+            -- map('v', '<leader>gr', function() gs.reset_hunk {vim.fn.line("."), vim.fn.line("v")} end)
+            -- map('n', '<leader>gS', gs.stage_buffer)
+            -- map('n', '<leader>gu', gs.undo_stage_hunk)
+            -- map('n', '<leader>gR', gs.reset_buffer)
             map('n', '<leader>gp', gs.preview_hunk)
-            -- map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-            -- map('n', '<leader>tb', gs.toggle_current_line_blame)
+            -- map('n', '<leader>gb', function() gs.blame_line{full=true} end)
+            map('n', '<leader>gb', gs.toggle_current_line_blame)
             map('n', '<leader>gd', gs.diffthis)
             -- map('n', '<leader>hD', function() gs.diffthis('~') end)
             -- map('n', '<leader>td', gs.toggle_deleted)
