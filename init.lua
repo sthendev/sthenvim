38832b4b9b1a076c717fbd346f8ace90e4031420
Sanju Thenuwara <sthendev@gmail.com>
2023-06-14 01:41:33 +1000

    move require project module higher

--- init.lua
+++ init.lua
@@ -2,10 +2,9 @@ local utils = require('utils')
 
 require('plugins')
 require('settings')
+require('project')
 require('keybindings')
 require('commands')
 require('colorscheme')
 
 utils.prequire('work')
-
-require('project')
