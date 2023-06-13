22bfb6be2ad7ce12b92e517b9a66b1e535470e65
Sanju Thenuwara <sthendev@gmail.com>
2023-06-14 01:05:46 +1000

    add custom highlights

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,4 +1,13 @@
 vim.cmd('colorscheme embark')
-vim.cmd('highlight clear DiffDelete')
-vim.cmd('highlight DiffDelete ctermbg=203 guibg=#411e35')
+
+local highlights = {
+    {'DiffDelete', {ctermbg=203, bg='#411e35'}},
+    {'LeapLabelPrimary', {fg='#100e23', bg='#a1efd3', ctermfg=232, ctermbg=120}},
+    {'LeapLabelSecondary', {fg='#100e23', bg='#d4bfff', ctermfg=232, ctermbg=143}},
+}
+
+for _, highlight in ipairs(highlights) do
+    local name, settings = unpack(highlight)
+    vim.api.nvim_set_hl(0, name, settings)
+end
 
