f33ae6f81415d80565baa03f4d5f8dfd7e7e2bac
Sanju Thenuwara <sthendev@gmail.com>
2023-07-09 15:42:08 +1000

    update custom highlights

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,9 +1,10 @@
 vim.cmd('colorscheme embark')
 
 local highlights = {
-    {'DiffDelete', {ctermbg=203, bg='#411e35'}},
+    {'DiffDelete', {bg='#411e35'}, ctermbg=203},
     {'LeapLabelPrimary', {fg='#100e23', bg='#a1efd3', ctermfg=232, ctermbg=120}},
     {'LeapLabelSecondary', {fg='#100e23', bg='#d4bfff', ctermfg=232, ctermbg=143}},
+    {'DiagnosticSignOk', {fg="#a1efd3", ctermfg=120}}
 }
 
 for _, highlight in ipairs(highlights) do
