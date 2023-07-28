925fe3135ba992a8245e737c7ae4bd67c7367912
Sanju Thenuwara <sthendev@gmail.com>
2023-07-28 18:57:11 +1000

    always close terminal even if error

--- lua/setup/floaterm.lua
+++ lua/setup/floaterm.lua
@@ -5,6 +5,7 @@ local settings = require('settings')
 function M.setup()
     settings.set_global('floaterm_width', 0.8)
     settings.set_global('floaterm_height', 0.8)
+    settings.set_global('floaterm_autoclose', 2)
 end
 
 return M
