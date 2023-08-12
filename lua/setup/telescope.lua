f04b2ee73270a1ae135e93731290eacd6046e4a4
Sanju Thenuwara <sthendev@gmail.com>
2023-08-12 14:32:25 +1000

    telescope: custom send_to_quickfix

--- lua/setup/telescope.lua
+++ lua/setup/telescope.lua
@@ -1,12 +1,23 @@
 local M = {}
 
+local telescope_actions = require('telescope.actions')
+
+local function send_to_quickfix(promptbufnr)
+    telescope_actions.smart_send_to_qflist(promptbufnr)
+    vim.cmd([[botright copen]])
+end
+
 function M.setup()
     require('telescope').setup{
         defaults = {
             mappings = {
+                n = {
+                    ['<C-q>'] = send_to_quickfix,
+                },
                 i = {
                     ['<C-j>'] = 'move_selection_next',
                     ['<C-k>'] = 'move_selection_previous',
+                    ['<C-q>'] = send_to_quickfix,
                 }
             }
         }
